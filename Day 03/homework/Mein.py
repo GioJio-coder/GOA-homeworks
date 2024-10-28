0ee4aee92e967deb69d72f64615809af8549cfc9
GioJio-coder <jioshvili90@gmail.com>
2024-10-28 21:03:43 +0400

    commit

---
+++ Day 03/homework/Mein.py
@@ -0,0 +1,40 @@
+print("Giorgi")
+print("GOA")
+print('GOA best')
+
+print(10)
+print(15)
+print(20)
+print(25)
+print("Giorgi")
+print("GOA")
+print('GOA best')
+
+print(10)
+print(15)
+print(20)
+print(25)
+
+print("Giorgi")
+print("Jioshvili")
+print(13)
+
+print("name")
+print("surname")
+print("age")
+
+fullname = "Giorgi Jioshvili"
+
+print(fullname)
+print(fullname)
+print(fullname)
+print(fullname)
+
+
+fullname = "Giorgi Jioshvili 100"
+fullname = "Giorgi Jioshvili 100"
+fullname = "Giorgi Jioshvili 100"
+fullname = "Giorgi Jioshvili 100"
+
+num = 100
+print(num)
