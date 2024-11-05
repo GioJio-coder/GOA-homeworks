b500c3e0d093c24be34c05f618791664f8dc87a1
GioJio-coder <jioshvili90@gmail.com>
2024-11-05 20:49:24 +0400

    commit

---
+++ Day 08/Classwork/lesson 8.py
@@ -0,0 +1,16 @@
+name = input(type(" enter your name: "))
+surname = input(type(" enter your surname: "))
+age = input(type(" enter yout age: "))
+number1 = input(type("enter the first number: "))
+number2 = input(type("enter the second number: "))
+
+print("name:" , name)
+print("surname:" , surname )
+print("age:" , age)
+print("number1:" , number1)
+print("number2:" , number2)
+
+
+number1 = input("Enter the first number: ")
+number2 = input("Enter the second number: ")
+print(number1 + number2)
---
+++ Day 08/Homework/Homework 9.py
@@ -0,0 +1,19 @@
+სულ python-ში ჩვენ გვაქვს 2 ნაკადი.ესენია ინფორმაციის შემოტანა input და ინფორმაციის გამოტანა output.ჩვენ ვიყინებთ print ბრაძანებას.
+
+input("hello enter your name: ")
+name = input(type("hello enter your name: "))
+print("name:" , name)
+
+input("hello enter your surname: ")
+surname = input(type("hello enter your surname: "))
+print("surname:" , surname)
+
+input("hello enter your name and surname: ")
+name surname = (type("hello enter your name and surname: "))
+print("name surname:" , name surname)
+
+print(name)
+print(surname)
+print(name surname)
+
+print((num1 + num2) / num3 - num4*num5 + num1**num3 % num2)
