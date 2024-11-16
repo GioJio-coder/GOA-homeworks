56eb98e3ef4c40e75d024e41f722f71ae888e451
GioJio-coder <jioshvili90@gmail.com>
2024-11-17 00:55:24 +0400

    commit

---
+++ Day 12/Classwork/Claswork 12.py
@@ -0,0 +1,16 @@
+print("ან ოპერატორი")
+print(True or False) # True
+print(False or True) # True
+print(True or True) # True
+print(False or False) # False
+
+print("და ოპერატორი")
+print(True and  False) # False
+print(False and True) # False
+print(True and True) # True
+print(False and  False) # False
+
+num1 = int(input("enter number one: "))
+num2 = int(input("enter number two: "))
+print(num1>30)
+print(num2<40)
