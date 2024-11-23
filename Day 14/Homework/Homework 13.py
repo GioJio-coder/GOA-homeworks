0553fa54de2b0938dcd64488b723a4250c723c13
GioJio-coder <jioshvili90@gmail.com>
2024-11-23 20:56:46 +0400

    commit

---
+++ Day 14/Homework/Homework 13.py
@@ -0,0 +1,56 @@
+იტერაცია - განმეორება
+# ციკლი - არის რაღაცის განმეორებით შესრულებას და ციკლის დავალება არის განმეორებით შეასრულოს ერთი და იგივე დავალება
+
+for x in range(3):
+    print(x*5)
+
+num1 = range(20, 51)
+
+num2 = range(100, 151)
+
+num3 = range(20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48)
+
+num4 = range(11, 13, 15, 17, 19, 21, 23, 35, 37, 29)
+
+for x in range(10):
+    print("Giorgi")
+    print("Jioshvili")
+
+print(6, 7, 8, 9, 10, 11, 12, 13, 14)
+
+print(40**3, 41**3, 42**3, 43**3,  44**3, 45**3, 46**3, 47**3, 48**3, 49**3, 50**3, 51**3, 52**3, 53**3, 54**3, 55**3, 56**3, 57**3, 58**3, 59**3)
+
+(input("Enter your number: "))
+print("Number")
+
+(input("Enter your number second: "))
+print("Number 2")
+
+(input("Enter your number third: "))
+print("Number 3")
+
+(input("Enter your number fourth: "))
+print("Number 4")
+
+(input("Enter your number fiveth: "))
+print("Number 5")
+for x in range(5):
+    print("Number")
+    print("Number 2")
+    print("Number 3")
+    print("Number 4")
+    print("Number 5")
+
+(input("Enter your name"))
+for x in name:
+    print(x)
+
+num1 = int(input("enter your number first: "))
+num2 = int(input("enter your number second: "))
+num3 = int(input("enter your number third: "))
+for x in range(num1, num2, num3):
+    print(num1**2, num2**2, num3**2)
+
+sum = 0
+
+# ამ ყველაფრის ხელით წერა იქნებოდა დამღლელი რადგან უფრო მეტ დროს და მუშაობას მოვუნდემოდით. ჩვენ ამის გასაკეთებლად ხელით დავწერდით ან copy, paste-ით, თუ ერთში გვექნებოდა შეცდომა მაშინ სხვებშიც გვექნებოდა თუ გავაკეთებდით copy, paste-ით და თუ კი ხელით დავწერდით უფრო მეტ დროს და მუშაობას მოვუნდემოდით.
