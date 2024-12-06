d20c3c9e438110e1ce54317d7975b542714d539c
GioJio-coder <jioshvili90@gmail.com>
2024-12-06 21:51:52 +0400

    commit

---
+++ Day 18/Homework/homework 15.py
@@ -0,0 +1,26 @@
+# პირობის შედეგი ყოველთვის არის ბულეანი.
+
+int(input("Enter your number: "))
+int(input("Enter your second number: "))
+# print("only the greatest number")
+
+int(input("Enter number: "))
+print(str, "is even")
+print(str, "is odd")
+
+int(input("Enter number: "))
+print(str, "is positive")
+print(str, "is negative")
+
+correct_password = "Goa best"
+user_guess = input("Enter your correct password: ")
+while user_guess != correct_password:
+    user_guess = input("Enter your correct password: ")
+
+    print("Access granted")
+    print("Your guess count:", str(counter))
+
+int(input("Enter your number: "))
+int(input("Enter your second number: "))
+
+print("Sum of all numbers are:", str)
