7047e5cea6e3516b2b77b8f9ed93eed172957d14
GioJio-coder <jioshvili90@gmail.com>
2024-12-13 20:50:20 +0400

    commit

---
+++ Day 20/Classwork/Claswork 16.py
@@ -0,0 +1,19 @@
+#      0  1  2  3  4  5  6  7  8  9
+list1=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+num1 = 0
+num2 = 1
+num3 = 2
+num4 = 3
+num5 = 4
+num6 = 5
+num7 = 6
+num8 = 7
+num9 = 8
+num10 = 9
+
+num1 = int(input("Enter first number"))
+num2 = int(input("enter second number"))
+num3 = int(input("enter third number"))
+
+list2 = [num1, num2, num3]
+print(list2)
---
+++ Day 20/Homework/Homework 17.py
@@ -0,0 +1,18 @@
+int(input("enter your first number and get: "))
+int(input("enter your second number and get: "))
+int(input("enter your third number and get: "))
+# if program get this number:
+#    print(three the greatest numbers)
+
+
+int(input("enter your first number and get: "))
+# if program get this number:  
+#    print(positive, negative or zero numbers)
+
+#      0   1  2  3  4
+list1=[1, 2, 3, 4, 5]
+print(list1[1, 2, 3, 4, 5])
+
+#      0  1  2  3  4  5  6  7  8   9  10  11  12  13  14  15  16   17 18  19
+list1=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+print(list1[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
