c9c870246a138aa1cc5f3ce57fd530b70d422994
GioJio-coder <jioshvili90@gmail.com>
2025-01-31 16:45:41 +0400

    commit

---
+++ Day 34/Classwork/Claswork 26.py
@@ -0,0 +1,7 @@
+def remove_elements(main_list, indices_list):
+    for ind in indices_list:
+        main_list.pop(ind)
+
+
+def remove_one_element(main_list, ind):
+    main_list.pop(ind)
---
+++ Day 34/Homework/Homework 27.py
@@ -0,0 +1,19 @@
+def arguments(arguments):
+    print("Hello, World!")
+
+def arguments_parametrs(arguments, parametrs):
+    num1 = 3
+    num2 = 5
+    arguments = "num1 + num2"
+
+def return_statement(statement):
+    num3 = 3
+    statement = "num3 * 10"
+    print(result)
+
+
+def numbers(numbers):
+    if numbers %2 == even:
+        print(even)
+    elif numbers %2 == odd:
+        print(odd)
