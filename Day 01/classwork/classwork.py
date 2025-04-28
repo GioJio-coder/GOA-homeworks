c776832b1df6a34f99768a46412a4e6e4a3814be
GioJio-coder <jioshvili90@gmail.com>
2025-04-28 14:10:23 +0400

    commit

---
+++ Day 01/classwork/classwork.py
@@ -0,0 +1,33 @@
+# name = "giorgi"
+# #name არის ცვლადი
+# # = არის ცვლადისთვის მნიშვნელონის მინიჭებული სიმბოლო
+# # "giorgi"  არის ცვლადისთვის მნიშვნელობა
+
+# # გამარჯობა მე ვარ გიორგი ჯიოშვილი. ვარ 13 წლის და ჩემი სიმაღლეა 143 სმ.
+# surname = "Jioshvili"
+
+
+# # prin( name )
+# #პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+# name = "giorgi" #ეს არის str ( string  ) ტიპის ცვლადი
+# age = 13 # ეს არის  ont ( integer )  მთელი რიცხვი 
+# height = 143. #ეს არის  float ტიპის ცვლადი (ათწილადი)
+# #boolean (bool) ტიპის ცვლადი
+
+# knows_programming = True   #True  ან  False 
+# is_ugly = False   #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+
+# isUgly = False  # ჯავასკრიპტული cameLcase
+
+# #სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# print(name + "  "+surname )
+# print(name + age)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+# print(name + " " + str(age))
