a7f60b91264aed862bc8c39a7336daba098fd470
GioJio-coder <jioshvili90@gmail.com>
2025-05-17 21:44:07 +0400

    commit

---
+++ Day 64/Classwork/home.js
@@ -0,0 +1,12 @@
+function Submit() {
+    let input = document.getElementById('username');
+    console.log(input.value); }
+
+
+    function concStrings() {
+        let first = prompt("შეიყვანე პირველი სტრინგი:");
+        let second = prompt("შეიყვანე მეორე სტრინგი:");
+
+        let sum = first + second;
+
+        alert("კონკატენაციის შედეგი: " + sum);}
