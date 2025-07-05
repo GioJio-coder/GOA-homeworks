fe8538f5b926e793805bb985276fc44291e96535
GioJio-coder <jioshvili90@gmail.com>
2025-07-05 17:31:09 +0400

    commit

---
+++ Day 80/Group.project/index.js
@@ -0,0 +1,21 @@
+// getting games with ids and making the images interactive
+let eldenRing=document.getElementById("elden")
+eldenRing.addEventListener("click",function(){
+    window.location.href="Elden-ring.html"
+})
+let counterStrike=document.getElementById("cs")
+counterStrike.addEventListener("click",function(){
+    window.location.href="Counter-Strike2.html"
+})
+let Minecraft=document.getElementById("minecraft")
+Minecraft.addEventListener("click",function(){
+    window.location.href="Minecraft.html"
+})
+let GTA=document.getElementById("gta")
+GTA.addEventListener("click",function(){
+    window.location.href="GTA.html"
+})
+let GranTurismo=document.getElementById("gran")
+GranTurismo.addEventListener("click",function(){
+    window.location.href="Gran-Turismo.html"
+})
