f078994c20d5b98eefb6f6cc4566194db5d6fef6
GioJio-coder <jioshvili90@gmail.com>
2025-08-13 13:03:39 +0400

    commit

---
+++ Day 92/Classwork/Classwork.py
@@ -0,0 +1,25 @@
+def capitalize(s):
+    even = [s[i].upper() if i % 2 == 0 else s[i] for i in range(len(s))  ]
+    odd = [s[i].upper() if i % 2 != 0 else s[i] for i in range(len(s))  ]
+    return ["".join(even),"".join(odd)]
+
+
+def no_odds(values):
+    return [x for x in values if x%2 == 0]
+
+
+def check_exam(arr1,arr2):
+    score = 0
+    for i in range(len(arr1)):
+        if arr2[i] == "":
+            score += 0
+        elif arr2[i] == arr1[i]:
+            score += 4
+        else: score-= 1
+    if score < 0:
+        return 0
+    return score
+
+
+def digits(n):
+    return len(str(n))
