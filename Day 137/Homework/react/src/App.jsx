638bfdf98a373f9d0ae193e08f8325366066cb52
GioJio-coder <jioshvili90@gmail.com>
2026-01-23 14:09:57 +0400

    commit

---
+++ Day 137/Homework/react/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Profile from "./Profile";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  )
+};
+
+export default App;
